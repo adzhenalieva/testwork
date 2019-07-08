d21c40c7fe2b3398115e7892e14f271ae6b715bf
Aisalkyn Djenalieva <aidzhenalieva@gmail.com>
2019-07-09 05:23:17 +0600

    Изменена адаптация меню

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
     $('#menu').click(function () {
         if ($('#menu').hasClass('mobile-menu')) {
             $('.main-nav').css("display", "block");
+            $('.profile-div').css("display", "block");
             $('#menu').addClass('mobile-menu-close').removeClass('mobile-menu');
         } else if ($('#menu').hasClass('mobile-menu-close')) {
             $('.main-nav').css("display", "none");
+            $('.profile-div').css("display", "none");
             $('#menu').addClass('mobile-menu').removeClass('mobile-menu-close');
         }
 
